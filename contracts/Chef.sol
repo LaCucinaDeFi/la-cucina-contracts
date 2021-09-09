9102b0868e2661c5865f2384157cc413af11f9e9
Ashitosh <ashitosh@deqode.com>
2021-09-09 17:45:00 +0530

    DV-1576: add Chef contract tests

--- contracts/Chef.sol
+++ contracts/Chef.sol
@@ -1,218 +1,209 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "./interfaces/IIngredientNFT.sol";
-import "./interfaces/IDishesNFT.sol";
-import "./interfaces/IVersionedContract.sol";
+import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155ReceiverUpgradeable.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import './interfaces/IIngredientNFT.sol';
+import './interfaces/IDishesNFT.sol';
+import './interfaces/IVersionedContract.sol';
 
 contract Chef is
-    AccessControlUpgradeable,
-    ReentrancyGuardUpgradeable,
-    IVersionedContract
+	AccessControlUpgradeable,
+	ReentrancyGuardUpgradeable,
+	IVersionedContract,
+	ERC1155ReceiverUpgradeable
 {
-    using Counters for Counters.Counter;
+	using Counters for Counters.Counter;
 
-    /*
+	/*
    =======================================================================
    ======================== Public Variables ============================
    =======================================================================
  */
-    IIngredientNFT public ingredientNft;
-    IDishesNFT public dishesNft;
+	IIngredientNFT public ingredientNft;
+	IDishesNFT public dishesNft;
 
-    uint256[] public uncookedDishIds;
+	uint256[] public uncookedDishIds;
 
-    /*
+	/*
    =======================================================================
    ======================== Constructor/Initializer ======================
    =======================================================================
  */
 
-    /**
-     * @notice Used in place of the constructor to allow the contract to be upgradable via proxy.
-     */
-    function initialize(
-        address _ingredientNftAddress,
-        address _dishesNftAddress
-    ) external virtual initializer {
-        require(
-            _ingredientNftAddress != address(0),
-            "Chef: INVALID_INGREDIENT_ADDRESS"
-        );
-        require(
-            _dishesNftAddress != address(0),
-            "Chef: INVALID_DISHES_ADDRESS"
-        );
+	/**
+	 * @notice Used in place of the constructor to allow the contract to be upgradable via proxy.
+	 */
+	function initialize(address _ingredientNftAddress, address _dishesNftAddress)
+		external
+		virtual
+		initializer
+	{
+		require(_ingredientNftAddress != address(0), 'Chef: INVALID_INGREDIENT_ADDRESS');
+		require(_dishesNftAddress != address(0), 'Chef: INVALID_DISHES_ADDRESS');
 
-        __AccessControl_init();
-        __ReentrancyGuard_init();
+		__AccessControl_init();
+		__ReentrancyGuard_init();
 
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+		_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 
-        ingredientNft = IIngredientNFT(_ingredientNftAddress);
-        dishesNft = IDishesNFT(_dishesNftAddress);
-    }
+		ingredientNft = IIngredientNFT(_ingredientNftAddress);
+		dishesNft = IDishesNFT(_dishesNftAddress);
+	}
 
-    /*
-   =======================================================================
-   ======================== Modifiers ====================================
-   =======================================================================
- */
-    modifier onlyAdmin() {
-        require(
-            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-            "Market: ONLY_ADMIN_CAN_CALL"
-        );
-        _;
-    }
-
-    /*
+	/*
    =======================================================================
    ======================== Public Methods ===============================
    =======================================================================
  */
-    /**
-     * @notice This method allows users to prepare a dish using more than 1 ingredients.
-     * @param _baseIngredientId - indicates the baseIngredient Id, depending on this it creates a dish.
-     * @param _ingredientIds - indicates the list of ingredients that you want to include in dish
-     * @return dishId - indicates the new dish id
-     */
-    function prepareDish(
-        uint256 _baseIngredientId,
-        uint256[] memory _ingredientIds
-    ) external returns (uint256 dishId) {
-        require(
-            _baseIngredientId > 0 &&
-                _baseIngredientId <= ingredientNft.getCurrentBaseIngredientId(),
-            "Chef: INVALID_BASE_INGREDIENT_ID"
-        );
-        require(
-            _ingredientIds.length > 1,
-            "DishesNFT: INSUFFICIENT_INGREDIENTS"
-        );
+	/**
+	 * @notice This method allows users to prepare a dish using more than 1 ingredients.
+	 * @param _baseIngredientId - indicates the baseIngredient Id, depending on this it creates a dish.
+	 * @param _ingredientIds - indicates the list of ingredients that you want to include in dish
+	 * @return dishId - indicates the new dish id
+	 */
+	function prepareDish(uint256 _baseIngredientId, uint256[] memory _ingredientIds)
+		external
+		returns (uint256 dishId)
+	{
+		require(
+			_baseIngredientId > 0 && _baseIngredientId <= ingredientNft.getCurrentBaseIngredientId(),
+			'Chef: INVALID_BASE_INGREDIENT_ID'
+		);
+		require(_ingredientIds.length > 1, 'DishesNFT: INSUFFICIENT_INGREDIENTS');
 
-        uint256 fats;
-        uint256 ingredientsHash;
-        uint256 currentIngredientId = ingredientNft.getCurrentNftId();
+		uint256 fats;
+		uint256 ingredientsHash;
+		uint256 currentIngredientId = ingredientNft.getCurrentNftId();
 
-        for (uint256 i = 0; i < _ingredientIds.length; i++) {
-            require(
-                _ingredientIds[i] > 0 &&
-                    _ingredientIds[i] <= currentIngredientId,
-                "Chef: INVALID_INGREDIENT_ID"
-            );
+		for (uint256 i = 0; i < _ingredientIds.length; i++) {
+			require(
+				_ingredientIds[i] > 0 && _ingredientIds[i] <= currentIngredientId,
+				'Chef: INVALID_INGREDIENT_ID'
+			);
 
-            // get the Ingredient NFT from user
+			// get the Ingredient NFT from user
 
-            ingredientNft.safeTransferFrom(
-                msg.sender,
-                address(this),
-                _ingredientIds[i],
-                1,
-                ""
-            );
+			ingredientNft.safeTransferFrom(msg.sender, address(this), _ingredientIds[i], 1, '');
 
-            (, , uint256 fat, ) = ingredientNft.ingredients(_ingredientIds[i]);
+			(, , uint256 fat, ) = ingredientNft.ingredients(_ingredientIds[i]);
 
-            fats += fat;
+			fats += fat;
 
-            // combine slotted ingredients into hash
-            ingredientsHash += _ingredientIds[i] * 256**i;
-        }
+			// combine slotted ingredients into hash
+			ingredientsHash += _ingredientIds[i] * 256**i;
+		}
 
-        // prepare the dish
-        dishId = dishesNft.prepareDish(
-            msg.sender,
-            _baseIngredientId,
-            fats,
-            _ingredientIds.length,
-            ingredientsHash
-        );
-    }
+		// prepare the dish
+		dishId = dishesNft.prepareDish(
+			msg.sender,
+			_baseIngredientId,
+			fats,
+			_ingredientIds.length,
+			ingredientsHash
+		);
+	}
 
-    /**
-     * @notice This method alloes users to uncook the dish by returning the dishNFT and claim back the ingredient nfts
-     * @param _dishId - indicates the id of dish to be uncooked.
-     */
-    function uncookDish(uint256 _dishId) external {
-        require(
-            _dishId > 0 && _dishId <= dishesNft.getCurrentNftId(),
-            "Chef: INVALID_DISH_ID"
-        );
+	/**
+	 * @notice This method alloes users to uncook the dish by returning the dishNFT and claim back the ingredient nfts
+	 * @param _dishId - indicates the id of dish to be uncooked.
+	 */
+	function uncookDish(uint256 _dishId) external {
+		require(_dishId > 0 && _dishId <= dishesNft.getCurrentNftId(), 'Chef: INVALID_DISH_ID');
 
-        // get details of dish
-        (
-            address dishHolder,
-            ,
-            ,
-            ,
-            uint256 totalIngredients,
-            uint256 ingredientsHash
-        ) = dishesNft.dish(_dishId);
+		// get details of dish
+		(address dishHolder, , , , uint256 totalIngredients, uint256 ingredientsHash) = dishesNft.dish(
+			_dishId
+		);
 
-        require(dishHolder == msg.sender, "Chef: ONLY_DISH_OWNER_CAN_UNCOOK");
+		require(dishHolder == msg.sender, 'Chef: ONLY_DISH_OWNER_CAN_UNCOOK');
 
-        // get the dish nft from user
-        dishesNft.safeTransferFrom(msg.sender, address(this), _dishId, 1, "");
+		// get the dish nft from user
+		dishesNft.safeTransferFrom(msg.sender, address(this), _dishId, 1, '');
 
-        uncookedDishIds.push(_dishId);
+		uncookedDishIds.push(_dishId);
 
-        // uncook the dish
-        dishesNft.uncookDish(_dishId);
+		// uncook the dish
+		dishesNft.uncookDish(_dishId);
 
-        uint256 slotConst = 256;
-        uint256 slotMask = 255;
-        uint256 bitMask;
-        uint256 slottedValue;
-        uint256 slotMultiplier;
-        uint256 variation;
+		uint256 slotConst = 256;
+		uint256 slotMask = 255;
+		uint256 bitMask;
+		uint256 slottedValue;
+		uint256 slotMultiplier;
+		uint256 variation;
 
-        // Iterate Ingredient hash by Gene and assemble SVG sandwich
-        for (uint8 slot = 0; slot <= uint8(totalIngredients); slot++) {
-            slotMultiplier = uint256(slotConst**slot); // Create slot multiplier
-            bitMask = slotMask * slotMultiplier; // Create bit mask for slot
-            slottedValue = ingredientsHash & bitMask; // Extract slotted value from hash
+		// Iterate Ingredient hash by Gene and assemble SVG sandwich
+		for (uint8 slot = 0; slot <= uint8(totalIngredients); slot++) {
+			slotMultiplier = uint256(slotConst**slot); // Create slot multiplier
+			bitMask = slotMask * slotMultiplier; // Create bit mask for slot
+			slottedValue = ingredientsHash & bitMask; // Extract slotted value from hash
 
-            if (slottedValue > 0) {
-                variation = (slot > 0) // Extract IngredientID from slotted value
-                    ? slottedValue / slotMultiplier
-                    : slottedValue;
+			if (slottedValue > 0) {
+				variation = (slot > 0) // Extract IngredientID from slotted value
+					? slottedValue / slotMultiplier
+					: slottedValue;
 
-                require(
-                    variation > 0 &&
-                        variation <= ingredientNft.getCurrentNftId(),
-                    "Chef: INVALID_INGREDIENT_VARIATION"
-                );
+				require(
+					variation > 0 && variation <= ingredientNft.getCurrentNftId(),
+					'Chef: INVALID_INGREDIENT_VARIATION'
+				);
 
-                // transfer the ingredient nft to user
-                ingredientNft.safeTransferFrom(
-                    address(this),
-                    msg.sender,
-                    variation,
-                    1,
-                    ""
-                );
-            }
-        }
-    }
+				// transfer the ingredient nft to user
+				ingredientNft.safeTransferFrom(address(this), msg.sender, variation, 1, '');
+			}
+		}
+	}
 
-    /**
-     * @notice Returns the storage, major, minor, and patch version of the contract.
-     * @return The storage, major, minor, and patch version of the contract.
-     */
-    function getVersionNumber()
-        external
-        pure
-        virtual
-        override
-        returns (
-            uint256,
-            uint256,
-            uint256
-        )
-    {
-        return (1, 0, 0);
-    }
+	/**
+	 * @notice Returns the storage, major, minor, and patch version of the contract.
+	 * @return The storage, major, minor, and patch version of the contract.
+	 */
+	function getVersionNumber()
+		external
+		pure
+		virtual
+		override
+		returns (
+			uint256,
+			uint256,
+			uint256
+		)
+	{
+		return (1, 0, 0);
+	}
+
+	function onERC1155Received(
+		address,
+		address,
+		uint256,
+		uint256,
+		bytes memory
+	) public virtual override returns (bytes4) {
+		return this.onERC1155Received.selector;
+	}
+
+	function onERC1155BatchReceived(
+		address,
+		address,
+		uint256[] memory,
+		uint256[] memory,
+		bytes memory
+	) public virtual override returns (bytes4) {
+		return this.onERC1155BatchReceived.selector;
+	}
+
+	/**
+	 * @dev See {IERC165-supportsInterface}.
+	 */
+	function supportsInterface(bytes4 interfaceId)
+		public
+		view
+		virtual
+		override(ERC1155ReceiverUpgradeable, AccessControlUpgradeable)
+		returns (bool)
+	{
+		return super.supportsInterface(interfaceId);
+	}
 }
--- test/Chef.test.js
+++ test/Chef.test.js
@@ -1,114 +1,287 @@
-const { expect } = require('chai')
-const { expectRevert, BN } = require('@openzeppelin/test-helpers')
-const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades')
-const { PizzaBase, pepper, tomato, mashroom } = require('./ingredientsData')
+require('chai').should();
+const {expect} = require('chai');
+const {expectRevert, BN} = require('@openzeppelin/test-helpers');
+const {deployProxy, upgradeProxy} = require('@openzeppelin/truffle-upgrades');
+const {PizzaBase, pepper, tomato, mashroom} = require('./ingredientsData');
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-require('chai').should()
+const Chef = artifacts.require('Chef');
+const IngredientNFT = artifacts.require('IngredientsNFT');
+const DishesNFT = artifacts.require('DishesNFT');
 
-const Chef = artifacts.require('Chef')
+const url = 'https://token-cdn-domain/{id}.json';
 
 contract('Chef', (accounts) => {
-  const owner = accounts[0]
-  const minter = accounts[1]
-  const user1 = accounts[2]
-  const user2 = accounts[3]
-  const user3 = accounts[4]
-  let dishId = 1
+	const owner = accounts[0];
+	const minter = accounts[1];
+	const user1 = accounts[2];
+	const user2 = accounts[3];
+	const user3 = accounts[4];
+	let dishId = 1;
 
-  before(async () => {
-    this.Chef = await deployProxy(Chef, [], {
-      initializer: 'initialize',
-    })
+	before(async () => {
+		this.Ingredient = await deployProxy(IngredientNFT, [url], {initializer: 'initialize'});
 
-    // add pizza base ingredients
-    await this.Chef.addBaseIngredient('PizzaBase', PizzaBase)
+		this.Dish = await deployProxy(DishesNFT, [url, this.Ingredient.address], {
+			initializer: 'initialize'
+		});
 
-    // add ingredients
-    await this.Chef.addIngredient('pepper', '100', pepper)
-    await this.Chef.addIngredient('tomato', '200', tomato)
-    await this.Chef.addIngredient('mashroom', '300', mashroom)
-  })
+		this.Chef = await deployProxy(Chef, [this.Ingredient.address, this.Dish.address], {
+			initializer: 'initialize'
+		});
 
-  it('should prepare dish with pepper, tomato and mashroom correctly', async () => {
-    // prepare dish
-    await this.Chef.prepareDish(1, [1, 2, 3])
+		// add pizza base ingredients
+		await this.Ingredient.addBaseIngredient('PizzaBase', PizzaBase);
 
-    const currentDishId = await this.Chef.getCurrentDishId()
+		// add ingredients
+		await this.Ingredient.addIngredient('pepper', url, '100', pepper);
+		await this.Ingredient.addIngredient('tomato', url, '200', tomato);
+		await this.Ingredient.addIngredient('mashroom', url, '300', mashroom);
 
-    // get dish
-    const dish = await this.Chef.serveDish(currentDishId)
+		// add minter in ingredient contract
+		const minterRole = await this.Ingredient.MINTER_ROLE();
+		await this.Ingredient.grantRole(minterRole, minter, {from: owner});
 
-    const addresssPath = await path.join(
-      'dishes',
-      'pizza' + dishId.toString() + '.svg',
-    )
-    dishId++
+		// add Chef contract as exceptedFrom address in ingredient
+		await this.Ingredient.addExceptedFromAddress(this.Chef.address, {from: owner});
 
-    await fs.writeFile(addresssPath, dish.toString(), (err) => {
-      if (err) throw err
-    })
-  })
+		// add Chef contract as excepted address in ingredient
+		await this.Ingredient.addExceptedAddress(this.Chef.address, {from: owner});
+	});
 
-  it('should prepare dish with pepper and tomato correctly', async () => {
-    // prepare dish
-    await this.Chef.prepareDish(1, [1, 2])
+	describe('initialize()', () => {
+		it('should initialize contracts correctly', async () => {
+			const ingredientAddress = await this.Chef.ingredientNft();
+			const dishesAddress = await this.Chef.dishesNft();
 
-    const currentDishId = await this.Chef.getCurrentDishId()
+			expect(ingredientAddress).to.be.eq(this.Ingredient.address);
+			expect(dishesAddress).to.be.eq(this.Dish.address);
+		});
 
-    // get dish
-    const dish = await this.Chef.serveDish(currentDishId)
+		it('should grant the admin role to deployer', async () => {
+			const adminRole = await this.Chef.DEFAULT_ADMIN_ROLE();
 
-    const addresssPath = await path.join(
-      'dishes',
-      'pizza' + dishId.toString() + '.svg',
-    )
-    dishId++
+			const isAdmin = await this.Chef.hasRole(adminRole, owner);
+			expect(isAdmin).to.be.eq(true);
+		});
+	});
 
-    await fs.writeFile(addresssPath, dish.toString(), (err) => {
-      if (err) throw err
-    })
-  })
+	describe('prepareDish()', async () => {
+		let user1PepperBalance;
+		let user1TomatoBalance;
+		let user1MashroomBalance;
 
-  it('should prepare dish with tomato and mashroom correctly', async () => {
-    // prepare dish
-    await this.Chef.prepareDish(1, [2, 3])
+		let chefPepperBalance;
+		let chef1TomatoBalance;
+		let chef1MashroomBalance;
 
-    const currentDishId = await this.Chef.getCurrentDishId()
+		let currentDishIdBefore;
 
-    // get dish
-    const dish = await this.Chef.serveDish(currentDishId)
+		before('setup contract for preparing dish', async () => {
+			// grant Chef role to Chef contract in Dish contract
+			const ChefRole = await this.Dish.CHEF_ROLE();
 
-    const addresssPath = await path.join(
-      'dishes',
-      'pizza' + dishId.toString() + '.svg',
-    )
-    dishId++
+			await this.Dish.grantRole(ChefRole, this.Chef.address, {from: owner});
 
-    await fs.writeFile(addresssPath, dish.toString(), (err) => {
-      if (err) throw err
-    })
-  })
+			// mint ingredients to the user1
+			await this.Ingredient.mint(user1, 1, 1, {from: minter});
+			await this.Ingredient.mint(user1, 2, 1, {from: minter});
+			await this.Ingredient.mint(user1, 3, 1, {from: minter});
 
-  it('should prepare dish with pepper and mashroom correctly', async () => {
-    // prepare dish
-    await this.Chef.prepareDish(1, [1, 3])
+			user1PepperBalance = await this.Ingredient.balanceOf(user1, 1);
+			user1TomatoBalance = await this.Ingredient.balanceOf(user1, 2);
+			user1MashroomBalance = await this.Ingredient.balanceOf(user1, 3);
 
-    const currentDishId = await this.Chef.getCurrentDishId()
+			chefPepperBalance = await this.Ingredient.balanceOf(this.Chef.address, 1);
+			chef1TomatoBalance = await this.Ingredient.balanceOf(this.Chef.address, 2);
+			chef1MashroomBalance = await this.Ingredient.balanceOf(this.Chef.address, 3);
 
-    // get dish
-    const dish = await this.Chef.serveDish(currentDishId)
+			//get current dish id
+			currentDishIdBefore = await this.Dish.getCurrentNftId();
+		});
 
-    const addresssPath = await path.join(
-      'dishes',
-      'pizza' + dishId.toString() + '.svg',
-    )
-    dishId++
+		it('should prepare dish correctly', async () => {
+			// approve ingredients to ChefContract
+			await this.Ingredient.setApprovalForAll(this.Chef.address, true, {from: user1});
 
-    await fs.writeFile(addresssPath, dish.toString(), (err) => {
-      if (err) throw err
-    })
-  })
-})
+			// prepare the dish
+			await this.Chef.prepareDish(1, [1, 2, 3], {from: user1});
+
+			// get users ingredient balance
+			const user1PepperBalanceAfter = await this.Ingredient.balanceOf(user1, 1);
+			const user1TomatoBalanceAfter = await this.Ingredient.balanceOf(user1, 2);
+			const user1MashroomBalanceAfter = await this.Ingredient.balanceOf(user1, 3);
+
+			// get Chef contract`s ingredient balance
+			const chefPepperBalanceAfter = await this.Ingredient.balanceOf(this.Chef.address, 1);
+			const chef1TomatoBalanceAfter = await this.Ingredient.balanceOf(this.Chef.address, 2);
+			const chef1MashroomBalanceAfter = await this.Ingredient.balanceOf(this.Chef.address, 3);
+
+			//get current dish id
+			const currentDishIdAfter = await this.Dish.getCurrentNftId();
+
+			//get user1`s dish balance
+			const dishBalance = await this.Dish.balanceOf(user1, currentDishIdAfter);
+
+			expect(dishBalance).to.bignumber.be.eq(new BN('1'));
+
+			expect(user1PepperBalance).to.bignumber.be.eq(new BN('1'));
+			expect(user1TomatoBalance).to.bignumber.be.eq(new BN('1'));
+			expect(user1MashroomBalance).to.bignumber.be.eq(new BN('1'));
+
+			expect(user1PepperBalanceAfter).to.bignumber.be.eq(new BN('0'));
+			expect(user1TomatoBalanceAfter).to.bignumber.be.eq(new BN('0'));
+			expect(user1MashroomBalanceAfter).to.bignumber.be.eq(new BN('0'));
+
+			expect(chefPepperBalance).to.bignumber.be.eq(new BN('0'));
+			expect(chef1TomatoBalance).to.bignumber.be.eq(new BN('0'));
+			expect(chef1MashroomBalance).to.bignumber.be.eq(new BN('0'));
+
+			expect(chefPepperBalanceAfter).to.bignumber.be.eq(new BN('1'));
+			expect(chef1TomatoBalanceAfter).to.bignumber.be.eq(new BN('1'));
+			expect(chef1MashroomBalanceAfter).to.bignumber.be.eq(new BN('1'));
+
+			expect(currentDishIdBefore).to.bignumber.be.eq(new BN('0'));
+			expect(currentDishIdAfter).to.bignumber.be.eq(new BN('1'));
+		});
+
+		it('should serve the prepared dish correctly', async () => {
+			//get current dish id
+			const currentDishId = await this.Dish.getCurrentNftId();
+
+			//get the svg of dish
+			const dishSvg = await this.Dish.serveDish(currentDishId);
+
+			const addresssPath = await path.join('dishes', 'pizza' + currentDishId.toString() + '.svg');
+			dishId++;
+
+			await fs.writeFile(addresssPath, dishSvg.toString(), (err) => {
+				if (err) throw err;
+			});
+		});
+
+		it('should revert when invalid baseIngredientId is used to prepare dish', async () => {
+			await expectRevert(
+				this.Chef.prepareDish(4, [1, 2, 3], {from: user1}),
+				'Chef: INVALID_BASE_INGREDIENT_ID'
+			);
+		});
+
+		it('should revert when no ingredient ids are used to prepare dish', async () => {
+			await expectRevert(
+				this.Chef.prepareDish(1, [], {from: user1}),
+				'DishesNFT: INSUFFICIENT_INGREDIENTS'
+			);
+		});
+
+		it('should revert when invalid ingredient id is used to prepare dish', async () => {
+			await expectRevert(
+				this.Chef.prepareDish(1, [5, 1, 3], {from: user1}),
+				'Chef: INVALID_INGREDIENT_ID'
+			);
+		});
+	});
+
+	describe('uncookDish()', async () => {
+		let userDishBalBefore;
+		let user1PepperBalance;
+		let user1TomatoBalance;
+
+		let chefPepperBalance;
+		let chef1TomatoBalance;
+		let chef1MashroomBalance;
+
+		let user1MashroomBalance;
+		let currentDishId;
+
+		before(async () => {
+			currentDishId = await this.Dish.getCurrentNftId();
+
+			// get user1`s dish balance
+			userDishBalBefore = await this.Dish.balanceOf(user1, currentDishId);
+
+			// get user1`s ingredient balance
+
+			user1PepperBalance = await this.Ingredient.balanceOf(user1, 1);
+			user1TomatoBalance = await this.Ingredient.balanceOf(user1, 2);
+			user1MashroomBalance = await this.Ingredient.balanceOf(user1, 3);
+
+			// get chef contract`s ingredient balance
+			chefPepperBalance = await this.Ingredient.balanceOf(this.Chef.address, 1);
+			chef1TomatoBalance = await this.Ingredient.balanceOf(this.Chef.address, 2);
+			chef1MashroomBalance = await this.Ingredient.balanceOf(this.Chef.address, 3);
+
+			// approve dish to ChefContract
+			await this.Dish.setApprovalForAll(this.Chef.address, true, {from: user1});
+
+			// uncook dish
+			await this.Chef.uncookDish(currentDishId, {from: user1});
+		});
+
+		it('should uncook dish correctly', async () => {
+			// get user1`s dish balance
+			const userDishBalAfter = await this.Dish.balanceOf(user1, currentDishId);
+
+			// get Chef contract`s dish balance
+			const ChefContractBal = await this.Dish.balanceOf(this.Chef.address, currentDishId);
+
+			// get users ingredient balance
+			const user1PepperBalanceAfter = await this.Ingredient.balanceOf(user1, 1);
+			const user1TomatoBalanceAfter = await this.Ingredient.balanceOf(user1, 2);
+			const user1MashroomBalanceAfter = await this.Ingredient.balanceOf(user1, 3);
+
+			// get Chef contract`s ingredient balance
+			const chefPepperBalanceAfter = await this.Ingredient.balanceOf(this.Chef.address, 1);
+			const chef1TomatoBalanceAfter = await this.Ingredient.balanceOf(this.Chef.address, 2);
+			const chef1MashroomBalanceAfter = await this.Ingredient.balanceOf(this.Chef.address, 3);
+
+			expect(userDishBalBefore).to.bignumber.be.eq(new BN('1'));
+			expect(userDishBalAfter).to.bignumber.be.eq(new BN('0'));
+
+			expect(ChefContractBal).to.bignumber.be.eq(new BN('1'));
+
+			expect(user1PepperBalance).to.bignumber.be.eq(new BN('0'));
+			expect(user1TomatoBalance).to.bignumber.be.eq(new BN('0'));
+			expect(user1MashroomBalance).to.bignumber.be.eq(new BN('0'));
+
+			expect(user1PepperBalanceAfter).to.bignumber.be.eq(new BN('1'));
+			expect(user1TomatoBalanceAfter).to.bignumber.be.eq(new BN('1'));
+			expect(user1MashroomBalanceAfter).to.bignumber.be.eq(new BN('1'));
+
+			expect(chefPepperBalance).to.bignumber.be.eq(new BN('1'));
+			expect(chef1TomatoBalance).to.bignumber.be.eq(new BN('1'));
+			expect(chef1MashroomBalance).to.bignumber.be.eq(new BN('1'));
+
+			expect(chefPepperBalanceAfter).to.bignumber.be.eq(new BN('0'));
+			expect(chef1TomatoBalanceAfter).to.bignumber.be.eq(new BN('0'));
+			expect(chef1MashroomBalanceAfter).to.bignumber.be.eq(new BN('0'));
+		});
+
+		it('should add the dish id uncooked dish ids list in Chef contract', async () => {
+			const uncookedDishIds = await this.Chef.uncookedDishIds(0);
+
+			expect(uncookedDishIds).to.bignumber.be.eq(currentDishId);
+		});
+
+		it('should revert when non-dishOwner tries to uncook the dish', async () => {
+			await expectRevert(
+				this.Chef.uncookDish(currentDishId, {from: user2}),
+				'Chef: ONLY_DISH_OWNER_CAN_UNCOOK'
+			);
+		});
+
+		it('should revert when dishOwner tries to uncook the dish with invalid dish id', async () => {
+			await expectRevert(this.Chef.uncookDish(0, {from: user1}), 'Chef: INVALID_DISH_ID');
+			await expectRevert(this.Chef.uncookDish(5, {from: user1}), 'Chef: INVALID_DISH_ID');
+		});
+
+		it('should revert when user wants to get svg of uncooked dish', async () => {
+			await expectRevert(
+				this.Dish.serveDish(currentDishId, {from: user1}),
+				'DishesNFT: CANNOT_SERVE_UNCOOKED_DISH'
+			);
+		});
+	});
+});
