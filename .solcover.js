675ad7eb995ddd107359992fe77dfde08fb47732
Ashitosh <ashitosh@deqode.com>
2021-11-25 17:16:17 +0530

    update .solcover

--- .solcover.js
+++ .solcover.js
@@ -7,7 +7,7 @@ module.exports = {
 		'mock/upgradable/DishesNFTV2.sol',
 		'mock/upgradable/IngredientNFTV2.sol',
 		'mock/upgradable/KitchenV2.sol',
-		'mock/upgradable/OvenV2.sol',
+		'mock/upgradable/CookerV2.sol',
 		'mock/upgradable/PrivateMarketplaceV2.sol',
 		'mock/upgradable/PublicMarketplaceV2.sol',
 		'mock/BlockData.sol',
